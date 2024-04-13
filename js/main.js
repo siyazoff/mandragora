2c7bbc33bf2983f7d2e3438d38dcfde873bd3790
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-04-13 21:20:21 +0500

    profile finished

--- js/main.js
+++ js/main.js
@@ -37,4 +37,78 @@ document.addEventListener("DOMContentLoaded", () => {
   if (document.querySelector("[data-tabs]")) {
     new Tabby("[data-tabs]");
   }
+
+  if (window.location.pathname.includes("settings")) {
+    const inputFile = document.querySelector("#profile__avatar");
+    const pictureImage = document.querySelector(".profile__avatar-desc");
+    const pictureImageTxt = "Выберите <br/> изображение";
+    pictureImage.innerHTML = pictureImageTxt;
+
+    inputFile.addEventListener("change", function (e) {
+      const inputTarget = e.target;
+      const file = inputTarget.files[0];
+
+      if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function (e) {
+          const readerTarget = e.target;
+
+          const img = document.createElement("img");
+          img.src = readerTarget.result;
+          img.classList.add("profile__avatar-img");
+
+          pictureImage.innerHTML = "";
+          pictureImage.appendChild(img);
+        });
+
+        reader.readAsDataURL(file);
+      } else {
+        pictureImage.innerHTML = pictureImageTxt;
+      }
+    });
+  }
+
+  if (window.location.pathname.includes("profile")) {
+    const inputFile = document.querySelector("#post-img");
+    const pictureImage = document.querySelector(".modal-add-post__span");
+    const pictureImageTxt = "Выберите <br/> изображение";
+    pictureImage.innerHTML = pictureImageTxt;
+
+    inputFile.addEventListener("change", function (e) {
+      const inputTarget = e.target;
+      const file = inputTarget.files[0];
+
+      if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function (e) {
+          const readerTarget = e.target;
+
+          const img = document.createElement("img");
+          img.src = readerTarget.result;
+          img.classList.add("modal-add-post__img");
+
+          pictureImage.innerHTML = "";
+          pictureImage.appendChild(img);
+        });
+
+        reader.readAsDataURL(file);
+      } else {
+        pictureImage.innerHTML = pictureImageTxt;
+      }
+    });
+
+    const modalCaller = document.querySelector(".modal-add-post__caller");
+    const modalClose = document.querySelector(".modal-add-post__close");
+    const modalWindow = document.querySelector(".modal-add-post");
+
+    modalCaller.addEventListener("click", function () {
+      modalWindow.classList.add("is_open");
+    });
+
+    modalClose.addEventListener("click", function () {
+      modalWindow.classList.remove("is_open");
+    });
+  }
 });
